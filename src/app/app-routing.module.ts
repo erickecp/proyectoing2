9abde0c5a67bf64ec3594d88b06ce42356567c83
JorgeMateos13 <mateosjorge026@gmail.com>
2023-06-02 07:37:11 -0600

    Sitema de rutas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const routes = [
+  {
+    path: '',
+    
+  }]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
