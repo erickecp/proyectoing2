b9f7c819ae2c09d90142bd24ce14023e84bf7390
JuanMMP99 <84467177+JuanMMP99@users.noreply.github.com>
2023-06-02 07:50:12 -0600

    Home creado
    Se creo el componente de home que muestra las cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PruebaAngularMaterialComponent } from './prueba-angular-material/prueba-angular-material.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
